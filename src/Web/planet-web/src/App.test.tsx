c44c1deb20a60ec1b38fc3c62d84cf6d23044df1
Ben Connolly <benconnolly5@gmail.com>
2023-07-30 14:02:47 +0100

    Updated page content and unit tests

--- src/Web/planet-web/src/App.test.tsx
+++ src/Web/planet-web/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders the page without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
+
+it('renders planets header', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Planets of the Solar/i);
+  expect(linkElement).toBeInTheDocument();});
