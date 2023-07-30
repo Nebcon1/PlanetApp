00ed2bb70d305990f5032ec4f682cc7cd6e43a2a
Ben Connolly <benconnolly5@gmail.com>
2023-07-30 19:29:53 +0100

    Separated PlanetList into it's own component

--- src/Web/planet-web/src/App.tsx
+++ src/Web/planet-web/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { IPlanet } from './Contracts/IPlanet';
+import { PlanetList } from './Components/PlanetList';
 
 function App(){
   return (
@@ -8,28 +8,6 @@ function App(){
         <PlanetList></PlanetList>
     </div>
   );
-}
-
-function PlanetList(){
-
-  const planets : IPlanet[] = [
-    {name:'Mercury', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Venus', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Earth', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Mars', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Jupiter', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Saturn', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Uranus', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
-    {name:'Neptune', diameter:1, distanceFromSun:1,mass:1,planetImage:''}
-  ]
-  return(
-    <>
-    <h2>Currently Classified as Planets:</h2>
-    <ul>
-      {planets.map((planet)=> <li>{planet.name}</li>)}
-    </ul>
-    </>
-  )
 }
 
 export default App;
---
+++ src/Web/planet-web/src/Components/PlanetList.tsx
@@ -0,0 +1,25 @@
+import { IPlanet } from "../Contracts/IPlanet"
+
+function PlanetList(){
+
+    const planets : IPlanet[] = [
+      {name:'Mercury', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Venus', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Earth', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Mars', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Jupiter', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Saturn', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Uranus', diameter:1, distanceFromSun:1,mass:1,planetImage:''},
+      {name:'Neptune', diameter:1, distanceFromSun:1,mass:1,planetImage:''}
+    ]
+    return(
+      <>
+      <h2>Currently Classified as Planets:</h2>
+      <ul>
+        {planets.map((planet)=> <li>{planet.name}</li>)}
+      </ul>
+      </>
+    )
+  }
+
+  export {PlanetList}
