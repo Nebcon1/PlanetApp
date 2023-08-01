38939e0977b06fef6f19286c8308849e5b1d489b
Ben Connolly <benconnolly5@gmail.com>
2023-08-01 23:35:27 +0100

    Added images and info card component

--- src/Web/planet-web/src/Components/ButtonFactory.tsx
+++ src/Web/planet-web/src/Components/ButtonFactory.tsx
@@ -1,13 +1,35 @@
 import { IPlanet } from "../Contracts/IPlanet";
 import Button from "./Button";
+import InformationCard from "./InformationCard";
 
-function ButtonFactory(planets: IPlanet[]){
-    return(
-    <>
-      {planets.map((planet)=> 
-      <Button style={{backgroundColor: 'navy', color: 'white'}} onClick={() => alert("Planet information placeholder.")}><span>{planet.name}</span></Button>)}
-    </>
-      )
+function ButtonFactory(planets: IPlanet[], ){
+    return (
+      <>
+        {planets.map((planet) => (
+          <Button
+            style={{
+              padding: "5px 50px",
+              textAlign: "center",
+              backgroundColor: "navy",
+              color: "white",
+            }}
+            onClick={() => handleClick(planet)}>
+            <span>{planet.name}</span>
+          </Button>
+        ))}
+      </>
+    );
   }
+
+function handleClick(planet: IPlanet){
+  return(
+    <InformationCard style={{
+      width: "300px",
+      border: "15px solid green",
+      padding: "50px",
+      margin: "20px"}}>
+    </InformationCard>
+  )
+};
 
   export default ButtonFactory
